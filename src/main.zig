433f1b516414d25d3aafe2a0bdecf1992f15a8c5
danielaguiar0006 <danielaguiar0006@gmail.com>
2024-10-24 22:27:45 -0700

    Add way to Generate First .ppm Image. Hello World!
    - To generate a .ppm image, run: zig build run > image.ppm

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,8 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
 const std = @import("std");
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    //std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
 
     // stdout is for the actual output of your application, for example if you
     // are implementing gzip, then only the compressed bytes should be sent to
@@ -14,24 +11,30 @@ pub fn main() !void {
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    // --- IMAGE ---
+    const imageWidth: u32 = 256;
+    const imageHeight: u32 = 256;
+    const maxColorValue: f32 = 255.999;
+
+    // --- RENDER ---
+    try stdout.print("P3\n{d} {d}\n255\n", .{ imageWidth, imageHeight });
+
+    for (0..imageHeight) |i| {
+        for (0..imageWidth) |j| {
+            const y: f32 = @floatFromInt(i);
+            const x: f32 = @floatFromInt(j);
+
+            const r: f32 = x / @as(f32, imageWidth - 1);
+            const g: f32 = y / @as(f32, imageHeight - 1);
+            const b: f32 = 0.0;
+
+            const ir: u32 = @intFromFloat(r * maxColorValue);
+            const ig: u32 = @intFromFloat(g * maxColorValue);
+            const ib: u32 = @intFromFloat(b * maxColorValue);
+
+            try stdout.print("{d} {d} {d}\n", .{ ir, ig, ib });
+        }
+    }
 
     try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const global = struct {
-        fn testOne(input: []const u8) anyerror!void {
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(global.testOne, .{});
 }
